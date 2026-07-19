5936f94d77b027e0efa0f166db162468f789c956
The-Mayor-BBQ <jacko672003@googlemail.com>
2026-07-19 20:14:11 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
+import "./styles/app.css";
+
 
 ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <App />
+document.getElementById("root")
+)
+.render(
+<App />
 );
