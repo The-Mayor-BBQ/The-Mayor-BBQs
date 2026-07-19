18267d9e4889a6e2e2b201cb340c2a4549e29500
The-Mayor-BBQ <jacko672003@googlemail.com>
2026-07-19 20:07:14 +0100

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,25 @@
+function App(){
+
+return (
+
+<div>
+
+<h1>
+👑 The Mayor BBQs
+</h1>
+
+<h2>
+Pitmaster Cookbook
+</h2>
+
+<p>
+Master the fire. Perfect the flavour.
+</p>
+
+</div>
+
+)
+
+}
+
+export default App;
