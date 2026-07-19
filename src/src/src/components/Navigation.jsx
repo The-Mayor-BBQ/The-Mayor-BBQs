6f295bad07e3adb3e154d5ab654fbd7282ab4ffb
The-Mayor-BBQ <jacko672003@googlemail.com>
2026-07-19 20:18:01 +0100

    Create Navigation.jsx

---
+++ src/src/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+function Navigation(){
+
+return (
+
+<nav className="navigation">
+
+<button>🏠 Home</button>
+
+<button>📖 Recipes</button>
+
+<button>❤️ Favourites</button>
+
+<button>🛒 Shopping List</button>
+
+<button>🔥 BBQ Tools</button>
+
+</nav>
+
+);
+
+}
+
+export default Navigation;
