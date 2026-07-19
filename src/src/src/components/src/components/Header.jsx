e32acda55004a390500f7077c30d2c8839b3b020
The-Mayor-BBQ <jacko672003@googlemail.com>
2026-07-19 20:18:40 +0100

    Create Header.jsx

---
+++ src/src/src/components/src/components/Header.jsx
@@ -0,0 +1,25 @@
+function Header(){
+
+return (
+
+<header>
+
+<h1>
+👑 The Mayor BBQs
+</h1>
+
+<p>
+Pitmaster Cookbook
+</p>
+
+<p>
+Master the fire. Perfect the flavour.
+</p>
+
+</header>
+
+);
+
+}
+
+export default Header;
